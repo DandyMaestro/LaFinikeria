2452e8b3da323a7e8a37b2831c9f69c5a026267e
DaNiELChIoRo <da.leon1917@gmail.com>
2020-01-19 19:45:18 -0600

    feat: adding the buttons feature to the swipe menu

--- app/src/main/java/com/danielchioro/lafinikeria/util/ItemSwipeController.java
+++ app/src/main/java/com/danielchioro/lafinikeria/util/ItemSwipeController.java
@@ -1,13 +1,21 @@
 package com.danielchioro.lafinikeria.util;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -17,7 +25,69 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.danielchioro.lafinikeria.R;
 
+import java.util.List;
+import java.util.Queue;
+
 public class ItemSwipeController extends ItemTouchHelper.Callback {
+
+    Context mContext;
+    private Paint mClearPaint;
+    private ColorDrawable mBackground;
+    private int backgroundColor;
+    private Drawable deleteDrawable;
+    private int intrinsicWidth;
+    private int intrinsicHeight;
+
+    private List<MyButton> buttonList;
+    private GestureDetector gestureDetector;
+    private Queue<Integer> removerQueue;
+    private int swipePosition = -1;
+    private RecyclerView recyclerView;
+    private GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            for(MyButton button: buttonList) {
+                if(button.onClick(e.getX(), e.getY()))
+                    break;
+            }
+            return true;
+        }
+    };
+
+    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(swipePosition < 0)return false;
+            Point point = new Point((int) motionEvent.getRawX(), (int) motionEvent.getRawY());
+
+            RecyclerView.ViewHolder swipeViewHolder = recyclerView.findViewHolderForAdapterPosition(swipePosition);
+            View swipedItem = swipeViewHolder.itemView;
+            Rect rect = new Rect();
+            swipedItem.getGlobalVisibleRect(rect);
+
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN ||
+            motionEvent.getAction() == MotionEvent.ACTION_UP ||
+            motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                if(rect.top < point.y && rect.bottom > point.y)
+                    gestureDetector.onTouchEvent(e);
+                else {
+                    removerQueue.add(swipePosition);
+                    swipePosition = -1;
+                    recoverSwipedItem();
+                }
+            }
+            return false;
+        }
+
+        private void recoverSwipedItem() {
+            while(!removerQueue.isEmpty()) {
+                int pos = removerQueue.poll();
+                if(pos > -1) {
+                    recyclerView.getAdapter().notifyItemChanged(pos);
+                }
+            }
+        }
+    };
 
     public ItemSwipeController(Context mContext) {
         this.mContext = mContext;
@@ -29,14 +99,6 @@ public class ItemSwipeController extends ItemTouchHelper.Callback {
         intrinsicWidth = deleteDrawable.getIntrinsicWidth();
         intrinsicHeight = deleteDrawable.getIntrinsicHeight();
     }
-
-    Context mContext;
-    private Paint mClearPaint;
-    private ColorDrawable mBackground;
-    private int backgroundColor;
-    private Drawable deleteDrawable;
-    private int intrinsicWidth;
-    private int intrinsicHeight;
 
     @Override
     public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
@@ -106,4 +168,61 @@ public class ItemSwipeController extends ItemTouchHelper.Callback {
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
 
     }
+
+    private class MyButton {
+        private String text;
+        private int imageResId, textSize, color, pos;
+        private RectF clickRegion;
+        private MyButtonClickListener listener;
+        private Context context;
+        private Resources resources;
+
+        public MyButton(String text, int imageResId, int textSize, int color, int pos, RectF clickRegion, MyButtonClickListener listener, Context context) {
+            this.text = text;
+            this.imageResId = imageResId;
+            this.textSize = textSize;
+            this.color = color;
+            this.pos = pos;
+            this.clickRegion = clickRegion;
+            this.listener = listener;
+            this.context = context;
+            resources = context.getResources();
+        }
+
+        public boolean onClick(float x, float y) {
+            if(clickRegion != null && clickRegion.contains(x, y)) {
+                listener.onClick(pos);
+                return true;
+            }
+            return false;
+        }
+
+        public void onDraw(Canvas c, RectF rectF, int pos) {
+            Paint p = new Paint();
+            p.setColor(color);
+            c.drawRect(rectF,p);
+
+            p.setColor(Color.WHITE);
+            p.setTextSize(textSize);
+
+            Drawable d = ContextCompat.getDrawable(context, imageResId);
+            Bitmap bitmap = drawableToBitmap(d);
+            c.drawBitmap(bitmap, (rectF.left+rectF.right)/2, (rectF.top+rectF.bottom)/2, p);
+
+            clickRegion = rectF;
+            this.pos = pos;
+        }
+
+        private Bitmap drawableToBitmap(Drawable d) {
+            if(d instanceof BitmapDrawable)
+                return((BitmapDrawable)d).getBitmap();
+            Bitmap bitMap = Bitmap.createBitmap(d.getIntrinsicWidth(),
+                    d.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitMap);
+            d.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            d.draw(canvas);
+            return bitMap;
+        }
+    }
 }
+
